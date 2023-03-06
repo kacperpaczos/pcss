10da9f55b687fd833d93bda88c084db16bf81b97
Kacper Paczos <kacper-paczos@linux.pl>
2023-03-06 17:10:59 +0100

    Add support for hours

--- SQL/pcss.sql
+++ SQL/pcss.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `pcss`.`odwiedziny` ( `id` INT NOT NULL AUTO_INCREMENT , `data` DATE NOT NULL , `adres` VARCHAR(15) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `pcss`.`odwiedziny` ( `id` INT NOT NULL AUTO_INCREMENT , `data` DATE NOT NULL , `godzina` VARCHAR(5) NOT NULL , `adres` VARCHAR(15) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
